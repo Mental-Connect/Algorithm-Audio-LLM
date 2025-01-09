477bd172f31318316d3a8f935a7e66ea36bff3d2
whyboy <1032131939@qq.com>
2025-01-09 12:39:35 +0800

    Audio-2.0, Fix the bug which missing the transcription

--- Service/services/offline_services.py
+++ Service/services/offline_services.py
@@ -19,7 +19,7 @@ class OfflineService:
         self.message_queue = queue.Queue()  # Queue to receive and process messages
         
         self.base_time = 0  # The transcripe end time.        
-        self.base_audio_data = bytes()
+        self.base_audio_cache = bytes()
 
         self.audio_sample_rate = 16000
         
@@ -42,73 +42,74 @@ class OfflineService:
 
     def __get_text_words(self, text: str):
         return re.findall(r"[a-zA-Z']+|[\u4e00-\u9fa5]", text)
-        
+    
 
-    def send_audio(self, audio_sample: bytes):
+    def __is_complete_sentence(self, sentence: str) -> bool:
+        sentence = sentence.strip()
+        return sentence.endswith(('.', '。', '?', '？'))
+
+
+    def send_audio(self, audio_new_data: bytes):
         """Process the audio sample using the offline model"""
         
         try:
-            logger.info(f"Processing audio sample of length: {len(audio_sample)}")
-            audio_data = self.base_audio_data + audio_sample
-            
+            logger.info(f"Processing audio sample of length: {len(audio_new_data)}")
+            audio_cache = self.base_audio_cache + audio_new_data
+            # print("audio_cache length: ", len(audio_cache))
             # The audio data can not be too small, otherwise it will generate issue.
-            if(len(audio_data) < 1000):
-                self.base_audio_data += audio_sample
+            if(len(audio_cache) < 1000):
+                self.base_audio_cache = audio_cache
                 return
             
-            result = offline_model(audio_data, model)
+            result = offline_model(audio_cache, model)
 
             if len(result) == 0 or result[0]['text'] == '':
                 self.send_heartbeat()
                 return
             
-            text = result[0]['text']
-            timestamp = result[0]['timestamp']
+            print("-----------------result: ", result)
+            text = result[0]['text']  
+            sentence_info_list = result[0]['sentence_info']
 
-            words = self.__get_text_words(text)
-            if (len(words) != len(timestamp)):
-                print(f"error, words count not equal timestamp count!, text: {text}, words: {words}, timestamp: {timestamp}")
-                raise
+            base_time = self.base_time
+            # 如果有多个语句，根据问号以及句号进行拆分
+            if len(sentence_info_list) > 1:
+                res_text = ''
 
-            # 如果句号的个数大于或等于2，则提取倒数第二个句号前的内容
-            period_count = self.__get_period_count(text)
-            if period_count >= 2:
-                res_text = self.__get_valid_text(text)
-                res_words = self.__get_text_words(res_text)
+                exist_complete_sentence = False
+                for idx, sentence_info in enumerate(sentence_info_list[0:-1]):   # 或略最后一个text
+                    
+                    tmp_text = sentence_info['text']                
+                    tmp_text_start_time = sentence_info['start']
+    
+                    res_text += tmp_text
+                    if self.__is_complete_sentence(res_text):
+                        exist_complete_sentence = True
+                        res_start_time = self.base_time
+                        res_end_time = base_time + tmp_text_start_time
+                        self.__send_message(res_text, res_start_time, res_end_time)
 
-                res_start_time = self.base_time
-                res_end_time = self.base_time + int(len(self.base_audio_data) / self.audio_sample_rate * 1000) + timestamp[len(res_words)-1][1]
+                        res_text = ''
+                        next_sentence_start = sentence_info_list[idx+1]['timestamp'][0][0]
+                        byte_to_skip = int(next_sentence_start / 1000 * self.audio_sample_rate * 2)   # each value contains 2 bytes, 16 bit.
+                        self.base_audio_cache = audio_cache[byte_to_skip::]
+                        self.base_time = res_end_time
+                
+                if not exist_complete_sentence:
+                    res_text = text
+                    res_start_time = self.base_time
+                    res_end_time = self.base_time + int(len(audio_cache) / self.audio_sample_rate * 1000)
+                    self.base_audio_cache = audio_cache
+                    self.__send_message(res_text, res_start_time, res_end_time)
 
-                self.base_time = res_end_time
-                byte_to_skip = int(timestamp[len(res_words)-1][1] / 1000 * self.audio_sample_rate) // 2 * 2
-                self.base_audio_data = audio_sample[byte_to_skip::]
             else:
                 res_text = text
                 res_start_time = self.base_time
-                res_end_time = self.base_time + int((len(self.base_audio_data) + len(audio_sample)) / self.audio_sample_rate * 1000)
-                self.base_audio_data += audio_sample
+                res_end_time = self.base_time + int(len(audio_cache) / self.audio_sample_rate * 1000)
+                # self.base_time = self.base_time + int(len(self.base_audio_cache) / self.audio_sample_rate * 1000)
 
-
-            if res_text != self.last_text: 
-                self.message_queue.put({
-                    "type": "TRANSCRIPT",  # Tag this message as transcribed text
-                    "text": res_text,
-                    "start_time": res_start_time,
-                    "end_time": res_end_time,
-                    "err": "OK"
-                })  # Put the transcribed text into the message queue
-                logger.info(f"Processed text: {text}")
-                print(f"Processed text: {text}")
-            else:
-                self.same_with_last_text_cnt += 1
-                if self.same_with_last_text_cnt >= 5:
-                    self.base_time = self.base_time + int(len(self.base_audio_data) / self.audio_sample_rate * 1000)
-                    self.same_with_last_text_cnt = 0
-                    self.base_audio_data = bytes()
-
-                self.send_heartbeat()
-
-            self.last_text = res_text
+                self.base_audio_cache = audio_cache
+                self.__send_message(res_text, res_start_time, res_end_time)
 
         except Exception as e:
             # Update the message queue with the error message if an exception occurs
@@ -120,9 +121,22 @@ class OfflineService:
                 "end_time": -1,
                 "err": f"Error: {error_message}"  # Include the error message
             })
-
+            print(f"Error processing audio sample: {e}")
             logger.error(f"Error processing audio sample: {e}")
             raise
+
+    # Send message to the queue
+    def __send_message(self, text, start_time, end_time):
+        if text != self.last_text:
+            self.message_queue.put({
+                    "type": "TRANSCRIPT",  # Tag this message as transcribed text
+                    "text": text,
+                    "start_time": start_time,
+                    "end_time": end_time,
+                    "err": "OK"
+                })
+            self.last_text = text
+
             
         
     def fetch_messages_from_queue(self):
