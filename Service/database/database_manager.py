64fffa716796117e13217c310397fc6f994a1582
south <2845231363@qq.com>
2025-06-04 16:20:37 +0800

    init structure

--- Service/database/database_manager.py
+++ Service/database/database_manager.py
@@ -14,4 +14,16 @@ class database_manager():
     # Update the data method
     def update_data():
         pass
+    
+    # 根据id查询学生
+    def get_school_user(id:str):
+        pass
+    
+    # 根据id查询管理员
+    def get_account_identity(id:str):
+        pass
+    
+    # 获取所有学校中包含的所有姓名
+    def get_names() -> {str,list[str]}:
+        pass
 
